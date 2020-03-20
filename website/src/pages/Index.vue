0b8e75d3be2e7a2bbd64aa013ed3f2c29a42c70c
VetulaMortem <renesebestian90@gmail.com>
2020-03-20 14:23:02 +0100

    First Commit

---
+++ website/src/pages/Index.vue
@@ -0,0 +1,45 @@
+<template>
+      <q-page>
+
+      <hr>
+      <form>
+        <input v-model="user.vname"/>
+        <input v-model="user.nname"/>
+        <input v-model="user.klasse"/> <input v-model="user.zweig"/></form>
+      <form v-on:submit.prevent="addNewUser">
+        <button type="submit">Add Schüler</button>
+      </form>
+      <ul>
+        <li v-for="user in users" :key="user.nname">
+          {{user.vname+' '+user.nname+' '+user.klasse+' '+user.zweig}}</li>
+      </ul>
+
+      </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      users: [],
+      user: {
+        vname: 'Vorname', nname: 'Nachname', klasse: 'Klasse', zweig: 'Zweig',
+      },
+    };
+  },
+  methods: {
+    addNewUser() {
+      this.users.push({
+        vname: this.user.vname,
+        nname: this.user.nname,
+        klasse: this.user.klasse,
+        zweig: this.user.zweig,
+      });
+    },
+  },
+
+};
+
+</script>
